0b73a2935391549e4f6f5106af692eabd8d80a62
R Moruju <morujurazvan@gmail.com>
2022-08-21 08:35:42 +0000

    Fix luck API

--- assets/js/luck.js
+++ assets/js/luck.js
@@ -1,17 +1,12 @@
-const axios = require("axios");
-
 const options = {
-  method: 'POST',
-  url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
-  params: {sign: 'aquarius', day: 'today'},
-  headers: {
-    'X-RapidAPI-Key': '934924768dmshb3321c772dbb901p1afa28jsna764ae2d255d',
-    'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
-  }
+	method: 'POST',
+	headers: {
+		'X-RapidAPI-Key': '934924768dmshb3321c772dbb901p1afa28jsna764ae2d255d',
+		'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+	}
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+fetch('https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));

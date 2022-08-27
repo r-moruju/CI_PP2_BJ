243088ef0a08933132d8c1df5bebaf2010d82b88
R Moruju <morujurazvan@gmail.com>
2022-08-27 08:48:04 +0000

    Add code to reset the contact form after submit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -6,18 +6,27 @@ close.addEventListener("click", closeModal);
 
 //Emailjs code
 (function() {
+    // Connect with emailjs
     emailjs.init('f7Jb5kjzdhjwsKmjT');
 })();
 window.onload = function() {
+    // Add event listener on window load
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
+        // Connect with emailjs related service
         emailjs.sendForm('service_9hcybw3', 'contact_form', this)
             .then(function() {
+                // Reset the form
+                resetForm();
                 console.log('SUCCESS!');
+                // Display modal
                 response.style.display = "unset";
                 responseMessage.innerText = "Your message has been sent";
             }, function(error) {
+                // Reset the form
+                resetForm();
                 console.log('FAILED...', error);
+                // Display modal
                 response.style.display = "unset";
                 responseMessage.innerText = "Message sent fail";
             });
@@ -29,4 +38,11 @@ window.onload = function() {
  */
 function closeModal() {
     response.style.display = "none";
+}
+
+/**
+ * Reset the Form
+ */
+function resetForm () {
+    document.getElementById("contact-form").reset();
 }
